a53517782bb1951d3791a006d5c5c815b96b0333
lopy2323 <r.keeren@hotmail.com>
2024-09-11 17:19:59 +0200

    1.0

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+
+
+let a = "Robert keeren"
+let b = "16"
+let c = "docent"
+
+console.log(a)
+console.warn(b)
+console.error(c)
---
+++ datum.js
@@ -0,0 +1,15 @@
+//nu
+const date = new Date()    
+//opdracht date
+const date1 = new Date(1985, 6, 12, 10, 39, 5, 10)
+const date2 = new Date(1955, 8, 23, 10, 39, 5, 10)
+const date3 = new Date(2015, 7, 31, 10, 39, 5, 10)
+const date4 = new Date(1885, 0, 4, 10, 39, 5, 10)
+//print de datums
+console.log("now time")
+console.log(date)
+console.log("past date")
+console.log(date1)
+console.log(date2)
+console.log(date3)
+console.log(date4)
