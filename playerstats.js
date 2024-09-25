2e21909749fac2ab85a22c2f862aa213855d64aa
lopy2323 <r.keeren@hotmail.com>
2024-09-25 16:33:57 +0200

    hallo

--- playerstats.js
+++ playerstats.js
@@ -1,24 +1,24 @@
 let Name = "robert keeren";
-let jumpforce = "jump force 15";
+let jumpforce = 15;
 let special_power = "power teleport";
 let speed = 280;
-let lives = "nummer of lives 1";
+let lives = 1;
 let ammo = 240;
-let nummerl = "number of lasers: 5";
-let battery = "battery power: 90";
-let lvl = "level 9000";
+let nummerl = 5;
+let battery = 90;
+let lvl = 9000;
 let transpot = "transport bike";
 let wanted = true;
 
 
 console.log(Name);
-console.log(jumpforce);
+console.log("jumpforce"+jumpforce);
 console.log(special_power);
 console.log(speed);
-console.log(lives);
+console.log("nummer of lives"+lives);
 console.log("ammo: "+ammo);
-console.log(nummerl);
-console.log(battery);
-console.log(lvl);
+console.log("nummer of lasers"+nummerl);
+console.log("batery power"+battery);
+console.log("level"+lvl);
 console.log(transpot);
 console.log(wanted);
